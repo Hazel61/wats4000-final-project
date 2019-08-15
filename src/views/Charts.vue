2bd21e5533c457ba65566947098bb4cb7916af4c
Rebecca Peltz <rebeccapeltz@gmail.com>
2019-08-15 10:15:57 -0700

    code to get charts running

--- src/views/Charts.vue
+++ src/views/Charts.vue
@@ -1,114 +1,132 @@
 <template>
-    <div>
-        <p>
-            High and low temperatures for 2018
-        </p>
-        <p>
-            <line-chart :options="chartOptions" :chartdata="chartdata"/>
-        </p>
-    </div>
+  <div>
+    <p>High and low temperatures for 2018</p>
+    <p>
+      <line-chart :options="chartOptions" :chart-data="chartData"/>
+    </p>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    import LineChart from '@/components/LineChart';
+import axios from 'axios';
+import LineChart from '@/components/LineChart';
 
-    export default {
-        name: 'Seattle',
-        data() {
+export default {
+  name: 'Seattle',
+  data () {
+    let a_dataset = [
+      {
+        label: 'High Temperature',
+        backgroundColor: '#f87979',
+        data: []
+      },
+      {
+        label: 'Low Temperature',
+        backgroundColor: 'blue',
+        data: []
+      }
+    ]
+    return {
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      chartData: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: a_dataset
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false
 
-            return {
-                chartdata: {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-                    datasets: [
-                        {
-                            label: 'High Temperature',
-                            backgroundColor: '#f87979',
-                            data: []
-                        },
-                        {
-                            label: 'Low Temperature',
-                            backgroundColor: 'blue',
-                            data: [],
-                        }
-                    ]
-                },
-                chartOptions: {
-
-                },
-                results: null,
-                errors: [],
-                Months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-            }
-        },
-
-        created() {
-            // High Temperature call
-            axios.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data', {
-                headers: {
-                    token: 'AlOCstuxmDrmZEVMyzYRHkJtzIOjDLBD',
-                },
-                params: {
-                    stationid: "GHCND:USW00024233",
-                    units: "standard",
-                    datatypeid: "EMXT",
-                    limit: 100,
-                    startdate: "2018-01-01",
-                    enddate: "2018-12-31",
-                    datasetid: "GSOM"
-                }
-            })
-                .then(response => {
-                    this.results = response.data.results;
-                    let highTemperatures = this.chartdata.datasets.find(dataset => dataset.label === 'High Temperature');
-                    if (highTemperatures) {
-                        highTemperatures.data = this.results.map(result => result.value);
-
-                    } else {
-                        console.error('Could not find high temperature dataset.');
-                    }
-
-                })
-                .catch(error => {
-                    this.errors.push(error)
-                });
-
-            // Low Temperature call
-            axios.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data', {
-                headers: {
-                    token: 'AlOCstuxmDrmZEVMyzYRHkJtzIOjDLBD',
-                },
-                params: {
-                    stationid: "GHCND:USW00024233",
-                    units: "standard",
-                    datatypeid: "EMNT",
-                    limit: 100,
-                    startdate: "2018-01-01",
-                    enddate: "2018-12-31",
-                    datasetid: "GSOM"
-                }
-            })
-                .then(response => {
-                    this.results = response.data.results;
-                    let lowTemperatures = this.chartdata.datasets.find(dataset => dataset.label === 'Low Temperature');
-                    if (lowTemperatures) {
-                        lowTemperatures.data = this.results.map(result => result.value);
-
-                    } else {
-                        console.error('Could not find high temperature dataset.');
-                    }
-                })
-                .catch(error => {
-                    this.errors.push(error)
-                });
-        },
-        components: {
-            // 'error-list': ErrorList,
-            'line-chart': LineChart
-        }
+      },
+      results: null,
+      errors: [],
+      Months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
     }
+  },
+
+  mounted () {
+    // High Temperature call
+    let lowTemperatures = null, highTemperatures = null
+    axios.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data', {
+      headers: {
+        token: 'AlOCstuxmDrmZEVMyzYRHkJtzIOjDLBD',
+      },
+      params: {
+        stationid: "GHCND:USW00024233",
+        units: "standard",
+        datatypeid: "EMXT",
+        limit: 100,
+        startdate: "2018-01-01",
+        enddate: "2018-12-31",
+        datasetid: "GSOM"
+      }
+    })
+      .then(response => {
+        this.results = response.data.results;
+        let highTemperatures = this.results.map(item => { return item.value })
+        //get low temps
+        // Low Temperature call
+
+        axios.get('https://www.ncdc.noaa.gov/cdo-web/api/v2/data', {
+          headers: {
+            token: 'AlOCstuxmDrmZEVMyzYRHkJtzIOjDLBD',
+          },
+          params: {
+            stationid: "GHCND:USW00024233",
+            units: "standard",
+            datatypeid: "EMNT",
+            limit: 100,
+            startdate: "2018-01-01",
+            enddate: "2018-12-31",
+            datasetid: "GSOM"
+          }
+        })
+          .then(response => {
+            this.results = response.data.results;
+
+            let lowTemperatures = this.results.map(item => { return item.value })
+
+            //put it all togther
+            let a_dataset = [
+              {
+                label: 'High Temperature',
+                backgroundColor: '#f87979',
+                data: highTemperatures
+              },
+              {
+                label: 'Low Temperature',
+                backgroundColor: 'blue',
+                data: lowTemperatures
+              }
+            ]
+
+            this.chartData = { labels: this.labels, datasets: a_dataset, chartOptions: this.chartOptions }
+
+          })
+          .catch(error => {
+            console.log("emnt", error)
+
+            this.errors.push(error)
+          });
+
+      })
+      .catch(error => {
+        console.log("emxt", error)
+        this.errors.push(error)
+      });
+
+
+  },
+  components: {
+    // 'error-list': ErrorList,
+    'line-chart': LineChart
+  },
+  
+}
 </script>
 
 <style scoped>
-
 </style>
